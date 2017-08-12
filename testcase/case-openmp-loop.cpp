5d0773d1702d65e5aa947c70e81da26c7544a2a4
Sebastien Valat <sebastien.valat@gmail.com>
2017-08-12 16:10:15 +0200

    [testcase] Improve OMP testcase

--- testcase/case-openmp-loop.cpp
+++ testcase/case-openmp-loop.cpp
@@ -9,40 +9,40 @@
 /********************  HEADERS  *********************/
 #include <cstdio>
 #include <cstdlib>
+#include <omp.h>
 
-/********************  GLOBALS  *********************/
-#define SIZE (200*1024*1024/sizeof(float))
+#define SIZE_PRE_THREAD (200*1024*1024/sizeof(float))
 
 /*******************  FUNCTION  *********************/
-void badFirstAccess()
+void badFirstAccess(size_t size)
 {
-	float * buffer = new float[SIZE];
+	float * buffer = new float[size];
 
 	//do first touch from main thread
-	for (size_t i = 0 ; i < SIZE ; i++)
+	for (size_t i = 0 ; i < size ; i++)
 		buffer[i] = i;
 	
 	//now do access in threads
 	#pragma omp parallel for
-	for (size_t i = 0 ; i < SIZE ; i++)
+	for (size_t i = 0 ; i < size ; i++)
 		buffer[i]++;
 	
 	delete [] buffer;
 }
 
 /*******************  FUNCTION  *********************/
-void betterFirstAccess()
+void betterFirstAccess(size_t size)
 {
-	float * buffer = new float[SIZE];
+	float * buffer = new float[size];
 
 	//do first touch from all threads in same way than access
 	#pragma omp parallel for
-	for (size_t i = 0 ; i < SIZE ; i++)
+	for (size_t i = 0 ; i < size ; i++)
 		buffer[i] = i;
 	
 	//now do access in threads
 	#pragma omp parallel for
-	for (size_t i = 0 ; i < SIZE ; i++)
+	for (size_t i = 0 ; i < size ; i++)
 		buffer[i]++;
 	
 	delete [] buffer;
@@ -51,11 +51,15 @@ void betterFirstAccess()
 /*******************  FUNCTION  *********************/
 int main()
 {
+	size_t threads = omp_get_num_threads();
+	size_t size = threads * SIZE_PRE_THREAD;
+	printf("Run on %lu threads\n",threads);
+	
 	printf("Running bad first touch...\n");
-	badFirstAccess();
+	badFirstAccess(size);
 	
 	printf("Running better first touch...\n");
-	betterFirstAccess();
+	betterFirstAccess(size);
 	
 	return EXIT_SUCCESS;
 }
