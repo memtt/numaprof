0241342ecae705d6bc2b10cb3dc9594d6920d365
Sébastien Valat <sebastien.valat@gmail.com>
2018-01-16 15:41:06 +0100

    [core] No, on current linux we still need not relative on main executable, need to trigger this automatically as it is not working on gentoo now

--- extern-deps/from-malt-v2/SymbolRegistry.cpp
+++ extern-deps/from-malt-v2/SymbolRegistry.cpp
@@ -274,7 +274,7 @@ void SymbolRegistry::solveNames(LinuxProcMapEntry * procMapEntry)
 	std::stringstream addr2lineCmd;
 	addr2lineCmd << "addr2line -C -f -e " << procMapEntry->file;
 	std::vector<CallSite*> lst;
-	bool isSharedLib = true;
+	bool isSharedLib = false;
 	
 	//check if shared lib or exe
 	if (procMapEntry->file.substr(procMapEntry->file.size()-3) == ".so" || procMapEntry->file.find(".so.") != std::string::npos)
