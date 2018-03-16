e230fdefe6455231b07671024a3d9c5294f35b3d
Sebastien Valat <sebastien.valat@gmail.com>
2018-03-16 21:31:01 +0100

    [test] Add bench to check efficiency of TLB versus page table walk (TLB win with factor 2)

---
+++ src/lib/tests/BenchTBLVsPageTable.cpp
@@ -0,0 +1,66 @@
+/*****************************************************
+             PROJECT  : numaprof
+             VERSION  : 1.1.0-dev
+             DATE     : 02/2018
+             AUTHOR   : Valat Sébastien - CERN
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include "../common/StaticAssoCache.hpp"
+#include "../core/PageTable.hpp"
+#include "../../../extern-deps/from-fftw/cycle.h"
+
+/********************  MACRO  ***********************/
+#define REPEAT 400
+#define SIZE 1024*1024
+
+/***************** USING NAMESPACE ******************/
+using namespace numaprof;
+
+/*********************  TYPES  **********************/
+typedef StaticAssoCache<Page,4,32> TLB;
+
+/*******************  FUNCTION  *********************/
+int main(int argc, char ** argv)
+{
+	//buf
+	int * buffer = new int[SIZE];
+	memset(buffer,0,SIZE*sizeof(int));
+	
+	//table & tlb
+	TLB tlb;
+	PageTable table;
+	
+	//first set page
+	for (int i = 0 ; i < SIZE ; i++)
+		table.getPage((size_t)&buffer[i]);
+	
+	//measure page table
+	ticks t0 = getticks();
+	for (int i = 0 ; i < SIZE ; i++)
+		for (int r = 0 ; r < REPEAT ; r++)
+			table.getPage((size_t)&buffer[i]);
+	ticks t1 = getticks();
+	printf("Page table : %llu\n",t1-t0);
+	
+	//measure with cache
+	t0 = getticks();
+	Page p;
+	for (int i = 0 ; i < SIZE ; i++)
+	{
+		for (int r = 0 ; r < REPEAT ; r++)
+		{
+			Page * page = tlb.get((size_t)&buffer[i]>>NUMAPROF_PAGE_OFFSET);
+			if (page == NULL)
+				tlb.set((size_t)&buffer[i]>>NUMAPROF_PAGE_OFFSET,&p);
+		}
+	}
+	t1 = getticks();
+	printf("TLB        : %llu\n",t1-t0);
+		
+	return EXIT_SUCCESS;
+}
