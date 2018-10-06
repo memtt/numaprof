e6efb67cacd519fcc6f9c75c9ec796917f9c58f7
Sebastien Valat <sebastien.valat@gmail.com>
2018-10-07 01:19:55 +0200

    [cache] Fix new file headers (i'm not anymore cern) (working on #4)

--- src/lib/caches/CpuCache.cpp
+++ src/lib/caches/CpuCache.cpp
@@ -2,7 +2,7 @@
              PROJECT  : numaprof
              VERSION  : 1.1.0-dev
              DATE     : 02/2018
-             AUTHOR   : Valat Sébastien - CERN
+             AUTHOR   : Valat Sébastien
              LICENSE  : CeCILL-C
 *****************************************************/
 
